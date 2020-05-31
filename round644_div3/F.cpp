0835b5fae548922264417c89e763945ab56a149f
yongkim <penubokim@gmail.com>
2020-05-31 11:48:33 +0900

    round644 F.cpp done using brute force

--- round644_div3/F.cpp
+++ round644_div3/F.cpp
@@ -19,6 +19,39 @@ int main() {
 			arr.push_back(x);
 		}
 
+		string sol;
+		bool found = true;
+		int d;
+
+		for (int i = 0; i < m; i++) {
+			sol = arr[0];
+			for (int j = 0; j < 26; j++) {
+				sol[i] = 'a' + j;
+				found = true;
+				for (int k = 0; k < n; k++) {
+					d = 0;
+					for (int l = 0; l < m; l++) {
+						if (sol[l] != arr[k][l])
+							d++;
+						if (d > 1) {
+							found = false;
+							break;
+						}
+					}
+				}
+				if (found)
+					break;
+			}
+			if (found)
+				break;
+		}
+
+		if (found) {
+			cout << sol << "\n";
+		} else {
+			cout << -1 << "\n";
+		}
+
 		
 	}
 	return 0;
