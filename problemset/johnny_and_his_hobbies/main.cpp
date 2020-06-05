4521bf525a0e02fbef916888924d6bfecd0e5557
yongkim <penubokim@gmail.com>
2020-06-05 17:13:29 +0900

    johnny and his hobbies failed in c++ wrong at test case 2

--- problemset/johnny_and_his_hobbies/main.cpp
+++ problemset/johnny_and_his_hobbies/main.cpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-int visited[1024];
-
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -17,32 +15,39 @@ int main() {
 		int n, maxn = 0;
 		cin >> n;
 		vector<int> a(n);
-		memset(visited, 0, sizeof(int) * 1024);
+		vector<int> b;
 
 		for (int i = 0; i < n; i++) {
 			cin >> a[i];
-			visited[a[i]] = 1;
 			maxn = max(maxn, a[i]);
 		}
+		sort(a.begin(), a.end());
+
 		bool flag;
 		for (int i = 1; i <= maxn; i++) {
+			b.clear();
 			flag = true;
 			for (int j = 0; j < n; j++) {
-				if (visited[a[j] ^ i] > 1 || visited[a[j] ^ i] == 0)
-					flag = false;
-				visited[a[j] ^ i] += 1;
+				b.push_back(a[j] ^ i);
+			}
+			if (b.size() == n) {
+				sort(b.begin(), b.end());
+				for (int i = 0; i < n; i++) {
+					if (b[i] != a[i])
+						flag = false;
+				}
+			} else {
+				flag = false;
 			}
 			if (flag) {
 				cout << i << "\n";
 				break;
-			} else {
-				memset(visited, 0, sizeof(int) * 1024);
-				for (int j = 0; j < n; j++)
-					visited[a[j]] = 1;
 			}
 		}
-		if (!flag)
+		if (!flag) {
 			cout << -1 << "\n";
+		}
+			
 	}
 
 }
